cdbf484c99d2cca424981064921c977406f7b3e5
Phillipe John Jules <phillipejohnjules@gmail.com>
2023-10-21 02:18:18 +0100

    just finished first half of lesson
    the first half of this lesson is all about the DOM and how
    to manipulate it using javascript.

---
+++ manipulation.js
@@ -0,0 +1,36 @@
+const container = document.querySelector('#container');
+
+const content = document.createElement('div');
+content.classList.add('content');
+content.textContent = 'This is the glorious text-content!';
+
+container.appendChild(content);
+
+const para = document.createElement('p');
+
+para.textContent = "Hey I'm red!";
+para.style.color = "red";
+
+container.appendChild(para);
+
+const header3 = document.createElement('h3');
+
+header3.textContent = "I'm a blue h3!";
+header3.style.color = "blue";
+
+container.appendChild(header3);
+
+const pinkBackground = document.createElement('div');
+
+pinkBackground.style.backgroundColor = "pink";
+pinkBackground.style.border = "1px solid black";
+container.appendChild(pinkBackground);
+
+const heading1 = document.createElement('h1');
+heading1.textContent = "I'm in a div";
+pinkBackground.appendChild(heading1);
+
+const para2 = document.createElement('p');
+
+para2.textContent = "ME TOO!";
+pinkBackground.appendChild(para2);
